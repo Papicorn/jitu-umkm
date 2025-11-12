df6258cd966cc175177bfa236d58d4063f8097d4
Papicorn <zacgqyt@gmail.com>
2025-11-13 05:40:02 +0700

    change redirect first opened

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -3,7 +3,7 @@ import InputLogin from "@/components/auth/login/InputLogin"
 
 export default function Login() {
   return (
-    <main className="min-h-screen bg-[#FFCA40]">
+    <main className="min-h-screen h-full bg-[#FFCA40]">
       <div className="w-full max-w-sm mx-auto flex-col pb-5">
         <Logo />
         <InputLogin />
