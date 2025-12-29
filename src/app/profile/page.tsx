b3735ca544590c57b16e47fadac6344898cadaad
Papicorn <zacgqyt@gmail.com>
2025-12-29 19:00:54 +0700

    Profile Page

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,15 +1,13 @@
 import NavtopDashboard from "@/components/dashboard/NavtopDashboard";
 import NavBot from "@/components/partials/NavBot";
-import NavigationKonsinyasiKon from "@/components/transaksi/pos/Navigation";
-import ProdukTransaksiPOS from "@/components/transaksi/pos/ProdukTransaksiPOS";
-import SearchFilterKonPos from "@/components/transaksi/pos/SearchFilterKonPos";
+import ButtonNavigation from "@/components/profile/ButtonNavigation";
 
 export default function ProfilePage() {
     return (
         <main className="min-h-dvh bg-[#F4F7FE]">
-            <NavtopDashboard title="Transaksi" showBackButton = {false} backUrl = "/dashboard" />
+            <NavtopDashboard title="Transaksi" showBackButton = {true} backUrl = "/dashboard" />
             <div className="w-full max-w-[430px] px-5 sm:px-4 mx-auto flex-col pt-4 pb-27 space-y-4">
-                
+                <ButtonNavigation />
             </div>
             <NavBot />
         </main>
--- src/components/profile/ButtonNavigation.tsx
+++ src/components/profile/ButtonNavigation.tsx
@@ -1,1 +1,53 @@
-export default function
+import Link from "next/link"
+import Image from "next/image"
+
+export default function ButtonNavigation() {
+    return (
+        <>
+            <div className="w-full">
+                <div className="space-y-2 flex flex-col">
+                    <Link href="/profile/kasir">
+                    <div className="bg-white rounded-lg shadow-sm border border-zinc-200 px-4 py-4 text-sm text-zinc-800">
+                        <div className="flex flex-row">
+                            <div className="relative w-5.5 mr-3">
+                                <Image src="/assets/image/profiles.png" alt="Profile icon" fill />
+                            </div>
+                            Profil
+                        </div>
+                    </div>
+                    </Link>
+                    <Link href="/laporan/konsinyasi">
+                    <div className="bg-white rounded-lg shadow-sm border border-zinc-200 px-4 py-4 text-sm text-zinc-800">
+                        <div className="flex flex-row">
+                            <div className="relative w-5.5 mr-3">
+                                <Image src="/assets/image/bahan-baku.svg" alt="Bahan Baku icon" fill />
+                            </div>
+                            Bahan Baku
+                        </div>
+                    </div>
+                    </Link>
+                    <Link href="/laporan/konsinyasi">
+                    <div className="bg-white rounded-lg shadow-sm border border-zinc-200 px-4 py-4 text-sm text-zinc-800">
+                        <div className="flex flex-row">
+                            <div className="relative w-5.5 mr-3">
+                                <Image src="/assets/image/settings.svg" alt="Pengatuan icon" fill />
+                            </div>
+                            Pengaturan
+                        </div>
+                    </div>
+                    </Link>
+                    <Link href="/laporan/konsinyasi">
+                    <div className="bg-white rounded-lg shadow-sm border border-[#F92424] px-4 py-4 text-sm text-zinc-800">
+                        <div className="flex flex-row">
+                            <div className="relative w-5.5 mr-3">
+                                <Image src="/assets/image/leave.svg" alt="Leave icon" className="shadow" fill />
+                            </div>
+                            <span className="text-[#F92424]">Keluar</span>
+                        </div>
+                    </div>
+                    </Link>
+                </div>
+            </div>
+        </>
+    )
+}
